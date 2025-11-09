475a94662a38df5510fc808fdbec458d658a3e53
Nils Badtke <nils.badtke@stud.h-da.de>
2025-11-09 13:19:01 +0100

    feat(logic): WIP

--- src/logic/RunSatisfactory.py
+++ src/logic/RunSatisfactory.py
@@ -1,9 +1,18 @@
 import os
 
+from logic.World import World
+
 
 class RunSatisfactory:
-    def __init__(self, os_type: str):
+    def __init__(self, os_type: str, savegame_path: str):
         self.os: str = os_type
+        self.savegame_path: str = savegame_path
+        self.backup_savegame_path: str = savegame_path + "-bak"
+
+    def load_world(self, world: World):
+        os.copy_file_range(self.savegame_path, self.backup_savegame_path)
+        os.remove(self.savegame_path)
+        os.copy_file_range(world.sanitized_folder_path, self.savegame_path)
 
     async def run(self):
         os.cmd("echo kek")  # TODO everything
